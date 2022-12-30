3589673f67472cdd0eead21d3cf773d7d1ea5955
danmondaini <danielmondaini@outlook.com>
2022-12-30 11:40:24 -0300

    feat: Migrations e Model User

---
+++ app/backend/src/database/migrations/20221230143044-create-users.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const sequelize = require("sequelize");
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', {
+      id: {
+        type: sequelize.INTEGER
+      },
+
+      username: {
+        type: sequelize.STRING
+      },
+
+      role: {
+        type: sequelize.STRING
+      },
+
+      email: {
+        type: sequelize.STRING
+      },
+
+      password: {
+        type: sequelize.STRING
+      },
+
+
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('users');
+  }
+}
